541931988bff70eb14728226ccfc3422d5ef9afe
Bruno Apel <bruno.apel@adp.com>
2021-03-21 19:24:52 -0300

    Setting up frontend properly

--- mcomix-tracker-fe/src/components/App.tsx
+++ mcomix-tracker-fe/src/components/App.tsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+	renderNavigation() {
+		return (<div>Navigation</div>);
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>My App!</h1>
+				{this.renderNavigation()}
 			</div>
 		);
 	}
--- mcomix-tracker-fe/webpack.config.js
+++ mcomix-tracker-fe/webpack.config.js
@@ -2,38 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/app.tsx',
-    devtool: 'inline-source-map',
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+    },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
+                // use: 'babel-loader',
                 exclude: /node_modules/,
             }
         ]
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-        fallback: {
-            assert: require.resolve("assert/"),
-            buffer: require.resolve("buffer/"),
-            crypto: require.resolve("crypto-browserify"),
-            http: require.resolve("stream-http"),
-            path: require.resolve("path-browserify"),
-            stream: require.resolve("stream-browserify"),
-            util: require.resolve("util/"),
-            zlib: require.resolve("browserify-zlib")
-        },
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'dist')
-    },
     plugins:[
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            filename: 'index.html',
+            template: './public/index.html'
         })
     ],
-    target: 'async-node'
+    devtool: 'inline-source-map'
 }
